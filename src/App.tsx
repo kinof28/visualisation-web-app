df1917b3b4d344c95ad2222aa134588bed03e99d
Abdelwahab <ab28fb@gmail.com>
2023-04-06 08:08:23 +0100

    starting the smily face

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
+const height = 520;
+const width = 900;
+const center_x = width / 2;
+const center_y = height / 2;
+const stroke_width = 10;
+const circle_color = "yellow";
+const little_circle_color = "black";
+const small_radius = 50;
+const small_circle_offset_x = 100;
+const small_circle_offset_y = 100;
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <svg height={height} width={width}>
+        <circle
+          cx={center_x}
+          cy={center_y}
+          stroke="black"
+          strokeWidth={stroke_width}
+          fill={circle_color}
+          r={center_y - stroke_width / 2}
+        ></circle>
+        <circle
+          cx={center_x - small_circle_offset_x}
+          cy={center_y - small_circle_offset_y}
+          fill={little_circle_color}
+          r={small_radius}
+        ></circle>
+        <circle
+          cx={center_x + small_circle_offset_x}
+          cy={center_y - small_circle_offset_y}
+          fill={little_circle_color}
+          r={small_radius}
+        ></circle>
+      </svg>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
