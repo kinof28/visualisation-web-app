710b4b16d71103c849cf2600a4bb6b5cd274cba9
Abdelwahab <ab28fb@gmail.com>
2023-04-07 05:23:57 +0100

    Face Drawing/WIP: mouth arc using D3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { arc } from "d3";
 
 const height = 520;
 const width = 900;
@@ -13,32 +11,41 @@ const little_circle_color = "black";
 const small_radius = 50;
 const small_circle_offset_x = 100;
 const small_circle_offset_y = 100;
+const mouth_radius = 180;
+const mouthArc = arc()
+  .innerRadius(mouth_radius - 20)
+  .outerRadius(mouth_radius)
+  .startAngle(Math.PI / 2)
+  .endAngle((Math.PI * 3) / 2)
+  .cornerRadius(5);
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <svg height={height} width={width}>
-        <circle
-          cx={center_x}
-          cy={center_y}
-          stroke="black"
-          strokeWidth={stroke_width}
-          fill={circle_color}
-          r={center_y - stroke_width / 2}
-        ></circle>
-        <circle
-          cx={center_x - small_circle_offset_x}
-          cy={center_y - small_circle_offset_y}
-          fill={little_circle_color}
-          r={small_radius}
-        ></circle>
-        <circle
-          cx={center_x + small_circle_offset_x}
-          cy={center_y - small_circle_offset_y}
-          fill={little_circle_color}
-          r={small_radius}
-        ></circle>
+        <g transform={`translate(${center_x},${center_y})`}>
+          <circle
+            stroke="black"
+            strokeWidth={stroke_width}
+            fill={circle_color}
+            r={center_y - stroke_width / 2}
+          ></circle>
+          <circle
+            cx={-small_circle_offset_x}
+            cy={-small_circle_offset_y}
+            fill={little_circle_color}
+            r={small_radius}
+          ></circle>
+          <circle
+            cx={small_circle_offset_x}
+            cy={-small_circle_offset_y}
+            fill={little_circle_color}
+            r={small_radius}
+          ></circle>
+
+          <path d={mouthArc()} />
+        </g>
       </svg>
     </div>
   );
